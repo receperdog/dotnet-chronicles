424ec199c34935a259e544877dd16d0e367b1ae6
Recep Erdogan <rerdogan@receperdogan.dev>
2024-11-05 00:52:13 +0300

    Rename the folder

---
+++ Fundamentals/PropBasics/Car.cs
@@ -0,0 +1,8 @@
+namespace OopBasics
+{
+    public class Car
+{
+    public string color {get; set;}   
+    public string brand{get; set;}
+}
+}
---
+++ Fundamentals/PropBasics/Person.cs
@@ -0,0 +1,15 @@
+namespace OopBasics
+{
+    public class Person{
+    string name;
+
+    public string Name{
+        get{
+            return name;
+        }
+        set{
+            name = value;
+        }
+    }
+}
+}
